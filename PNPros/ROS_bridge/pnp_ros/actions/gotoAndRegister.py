30bbd6845bc5281920b87531c23597bc14e53fe9
francescodelduchetto <francescodelduchetto@gmail.com>
2018-01-26 16:13:27 +0000

    il nome della variabile diocacca

--- PNPros/ROS_bridge/pnp_ros/actions/gotoAndRegister.py
+++ PNPros/ROS_bridge/pnp_ros/actions/gotoAndRegister.py
@@ -28,7 +28,7 @@ class gotoAndRegister(AbstractAction):
         rospy.loginfo("Waiting for result...")
 
         # start registering robot_pose and twist
-        rospy.ServiceProxy("/start_state_action_saver", PNPStartStateActionSaver)
+        starting_sp = rospy.ServiceProxy("/start_state_action_saver", PNPStartStateActionSaver)
         filename = goal_topo + str(rospy.Time.now().to_nsec())
         self.goal_id = filename
         folder = '%s/workspaces/museum_ws/data/passage_trajectories'  % os.path.expanduser("~")
