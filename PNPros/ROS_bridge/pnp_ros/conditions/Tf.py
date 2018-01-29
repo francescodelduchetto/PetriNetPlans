6853d8f4860c0fae6766f33f1b6061b09d39820c
francescodelduchetto <francescodelduchetto@gmail.com>
2018-01-29 11:22:27 +0000

    tf and tf static fix...

--- PNPros/ROS_bridge/pnp_ros/conditions/Tf.py
+++ PNPros/ROS_bridge/pnp_ros/conditions/Tf.py
@@ -8,9 +8,10 @@ class Tf(AbstractTopicCondition):
     _topic_type = TFMessage
 
     def _get_value_from_data(self, data):
-        tra = data.transforms.transform.translation
-        rot = data.transforms.transform.rotation
-        return (data.transforms.header.frame_id, data.transforms.child_frame_id, tra.x, tra.y, tra.z, rot.x, rot.y, rot.z, rot.w)
+        # TODO this just consider the first...
+        tra = data.transforms[0].transform.translation
+        rot = data.transforms[0].transform.rotation
+        return (data.transforms[0].header.frame_id, data.transforms[0].child_frame_id, tra.x, tra.y, tra.z, rot.x, rot.y, rot.z, rot.w)
 
     def evaluate(self, params):
         node = str(params[0])
